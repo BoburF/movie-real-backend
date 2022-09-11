29220535305ec916a311adc0ca9e4528fbc4d4e5
Bobur Abdullayev <changeitgo3@gmail.com>
2022-09-11 18:55:22 +0500

    feat: json

--- routes/client/index.js
+++ routes/client/index.js
@@ -5,7 +5,7 @@ const control = require('../../controller/client/index')
 router.get('/', control.movies)
 router.get('/:id', control.movieOne)
 router.get('/movie/:id', control.movieOneRedirect)
-router.post('/add', control.moviesAdd)
+
 
 
 module.exports = router
